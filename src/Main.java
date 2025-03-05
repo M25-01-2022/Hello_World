7962a700fbddd9c8ea8cab6398a54c083f98d6bc
M25-01-2022 <mariodavidtaipeflores@elpuig.xeill.net>
2025-03-05 19:28:22 +0100

    S'han creat 4 mètodes nous.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        merhod1();
+        merhod2();
+        merhod3();
+        merhod4();
+    }
+
+    private static void merhod1(){
+
+    }
+
+    private static void merhod2(){
+
+    }
+
+    private static void merhod3(){
+
+    }
+
+    private static void merhod4(){
+
+    }
+}
