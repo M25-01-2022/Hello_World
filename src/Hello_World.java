a3d120bdb994d3c722d872f431a0e17bd7c87730
M25-01-2022 <mariodavidtaipeflores@elpuig.xeill.net>
2025-03-05 19:20:51 +0100

    Primer commit.

---
+++ src/Hello_World.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Hello_World {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        System.out.print("Hello World");
+    }
+}
